9a75cad24fdca140edf3943f03c7bae616cea1d9
G0od1k <g0cd1k@gmail.com>
2024-07-25 06:01:23 +0200

    Changed "add image" process

--- images.js
+++ images.js
@@ -1,22 +1,23 @@
-function addImage(data) {}
+function addImage(data, id = "") {
+    document
+        .querySelector("#imgList")
+        .appendChild(document.querySelector("#tem-img").content.cloneNode(true))
+
+    const pageNode = document.querySelector("#imgList").lastElementChild
+
+    pageNode.querySelector(".del").onclick = () => {
+        document.querySelector("#imgList").removeChild(pageNode)
+    }
+    pageNode.querySelector("img").src = data
+    pageNode.querySelector(".imgId").value = id
+}
 
 document.querySelector("#imgFileInput").addEventListener("change", (e) => {
     const file = e.target.files[0]
     const reader = new FileReader()
 
     reader.onload = (e) => {
-        document
-            .querySelector("#imgList")
-            .appendChild(
-                document.querySelector("#tem-img").content.cloneNode(true)
-            )
-
-        const pageNode = document.querySelector("#imgList").lastElementChild
-
-        pageNode.querySelector(".del").onclick = () => {
-            document.querySelector("#imgList").removeChild(pageNode)
-        }
-        pageNode.querySelector("img").src = e.target.result
+        addImage(e.target.result)
     }
 
     reader.readAsDataURL(file)
